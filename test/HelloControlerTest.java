71697b09c8e7db895bddd210dc48c15d77597b83
JeanneBM <bojko.joanna@gmail.com>
2021-02-16 22:08:05 +0100

    TrialApp

---
+++ test/HelloControlerTest.java
@@ -0,0 +1,28 @@
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+public class HelloControllerTest{
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    public void beforeSetUp(){
+        this.mockMvc = standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void whenGivenAttributeNameThenItIsInModel() throws Exception{
+        this.mockMvc.perform(get("/greeting?name=Asia")
+                .accept(MediaType.ALL))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("name", "Asia"));
+    }
+
+}
